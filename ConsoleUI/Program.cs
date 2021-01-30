a5f3fe61e49364365e6ae252aa2a6053b511970d
hmtshbz8@gmail.com <hmtshbz8@gmail.com>
2021-01-31 00:10:12 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productmanager = new ProductManager(new InMemoryProductDal());
+            foreach (var product in productmanager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
